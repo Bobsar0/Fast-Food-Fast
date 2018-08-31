0a95ebe97f7968104763d936d5355a512874291b
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-08-31 23:19:02 +0100

    Enhance Home Page
    Add Slideshow images. Add FontAwesome icons to communicate About section

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,3 +1,24 @@
+/******SLIDESHOW***** */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1;}    
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+
 /****ADD TO CART IMPLEMENTATION****/
 let cartBtns = document.getElementsByClassName("cartBtn");
 const table = document.getElementById("table")
