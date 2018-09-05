be458edede3ee784723544bfd974c3e6dd7c0d19
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-05 14:31:06 +0100

    Pass real PostsController instance to server instance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import server from '../server';
+import servers from '../server';
+import OrdersController from '../controllers/ordersController';
+
+const orders = new OrdersController();
+const server = servers(orders);
 
 server.listen(8080, () => console.log('Server listening at port 8080...'));
