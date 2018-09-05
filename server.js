cead3f5efb52f1c6f0f8fe41504a71fd9907007a
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-05 14:02:37 +0100

    Modify server to use controller
    Prepare Server for Content Testing #2

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import OrdersController from './controllers/orders';
+
 
 const server = express();
+const orders = new OrdersController();
 
 // Server responds with the empty object for now. Content
 // is not tested yet, just server availability.
-server.get('/orders', (req, res) => res.send({}));
+server.get('/orders', (req, res) => orders.index().then(result => res.send(200, result)));
 
 export default server;
