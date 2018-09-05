dfdd87b332dcaabac85f85e383e068d806a5d329
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-05 13:13:52 +0100

    Configure basic server instance that responds to /orders  to pass test
    Configure server to respond to '/orders' with the empty object for now.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const server = express();
 
-// Server always responds with the empty object for now. Content
+// Server responds with the empty object for now. Content
 // is not tested yet, just server availability.
 server.get('/orders', (req, res) => res.send({}));
 
