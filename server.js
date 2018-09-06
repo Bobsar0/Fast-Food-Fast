8f4633ae6bfc3c41cc96c9258a5b30b7e19a3296
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-06 02:47:08 +0100

    Define READ action on server to pass test

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ export default (orders) => {
 
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
+
   server.get('/orders', (req, res) => orders.index().then(result => res.status(200).send(result)));
+  server.get('/orders/:orderId', (req, res) => orders.read(req.params.orderId).then(result => res.status(200).send(result)));
+
   server.post('/orders', (req, res) => orders.create(req.body.order).then(result => res.status(201).send(result)));
 
   return server;
