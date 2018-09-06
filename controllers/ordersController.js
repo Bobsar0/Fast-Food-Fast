bf69279b5ece39ac2fb753be38d9400c9c19c6f1
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-06 19:42:10 +0100

    Prepare controller for testing

--- controllers/ordersController.js
+++
@@ -1,7 +0,0 @@
-export default class {
-  index() {
-    // We can assume that controller will return some kind of promise
-    // because it will make requests to ES that are asynchronous
-    return new this.Promise((resolve, reject) => resolve({}));
-  }
-}
---
+++ test/ordersControllerTest.js
@@ -0,0 +1,6 @@
+import OrdersController from '../api/v1/controllers/ordersController';
+
+describe('OrdersController', () => {
+  const user = {};
+  const orders = new OrdersController(user);
+});
