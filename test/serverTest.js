5bd93aba820967d15862274c79abd161b3b64a21
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-06 02:45:53 +0100

    Create failing test for server GET /order/:orderId implementation
    Note that this does not detect orders with invalid ids.

--- test/serverTest.js
+++ test/serverTest.js
@@ -11,7 +11,7 @@ describe('server', () => {
   describe('GET /orders', () => {
     // test data that is returned by the orders controller stub
     const data = [{
-      orderId: '123', user: 'Steve', name: 'Chicken', price: 'NGN 1000.00',
+      orderId: '123', user: 'Steve', address: 'Andela Epic Tower', name: 'Chicken', price: 'NGN 1000.00',
     }];
 
     // test method now returns test data
@@ -31,7 +31,7 @@ describe('server', () => {
   describe('POST /orders', () => {
     // data sent to the server
     const data = [{
-      user: 'Anonymous', name: 'Burger', price: 'NGN 800',
+      user: 'Anonymous', address: 'Andela HQ', name: 'Burger', price: 'NGN 800',
     }];
 
     before(() => {
@@ -40,12 +40,31 @@ describe('server', () => {
         attrs)));
     });
 
-    it('responds with CREATED and returns content of the newly created order with ID', () => request
+    it('responds with CREATED and returns content of the newly created order with id attached', () => request
       .post('/orders')
       .send({ order: data })
       .expect(_.merge({ orderId: 234 }, data))
       .expect(201));
   });
+
+  // ======== GET /orders/:orderId TEST =================//
+  describe('GET /orders/:orderId', () => {
+    // data that is returned from the controller stub
+    const data = [{
+      orderId: '234', user: 'Steve', address: 'Andela Epic Tower', name: 'Chicken', price: 'NGN 1000.00',
+    }];
+
+    // The read action stub merges specified id with the predefined data to imitate real controller
+    // behaviour and checks that proper id was passed to the controller
+    before(() => {
+      orders.read = id => new Promise((resolve, reject) => resolve(_.merge({ orderId: id }, data)));
+    });
+
+    // checks that server just pass id to the controller and returns its result.
+    it('responds with OK and returns unique order corresponding to the id', () => request
+      .get('/orders/234')
+      .send(data)
+      .expect(_.merge({ orderId: 234 }, data))
+      .expect(200));
+  });
 });
-
-
