de693693cf2be32ed00a5068640adad363e65b52
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-07 03:36:24 +0100

    Create failing test for index method of ordersController
    Index method should returns list of orders - GET /orders

--- test/ordersControllerTest.js
+++ test/ordersControllerTest.js
@@ -1,6 +1,49 @@
+import { should } from 'should';
 import OrdersController from '../api/v1/controllers/ordersController';
 
 describe('OrdersController', () => {
-  const user = {};
-  const orders = new OrdersController(user);
+  // introdue external client stub
+  const client = {};
+  const orders = new OrdersController(client);
+
+  // index should return list of orders merged with corresponding ids
+  describe('index', () => {
+    before(() => {
+      client.search = () => new Promise((resolve, reject) => resolve({
+        orders: [{
+          orderId: 'ABCDEFGHIJKLMNO',
+          name: 'Chicken',
+          price: 1000,
+          username: 'Steve',
+          userAddr: 'Andela',
+          userRank: 'admin',
+        },
+        {
+          orderId: 'PQRSTUVWXYZABCDE',
+          name: 'Rice',
+          price: 800,
+          username: 'Anonymous',
+          userAddr: 'Somewhere',
+          userRank: 'guest',
+        }],
+      }));
+    });
+    // check to see if it correctly parses and returns post data
+    it('parses and returns post data', () => orders.index().then(result => result.should.deepEqual([{
+      orderId: 'ABCDEFGHIJKLMNO',
+      name: 'Chicken',
+      price: 1000,
+      username: 'Steve',
+      userAddr: 'Andela',
+      userRank: 'admin',
+    },
+    {
+      orderId: 'PQRSTUVWXYZABCDE',
+      name: 'Rice',
+      price: 800,
+      username: 'Anonymous',
+      userAddr: 'Somewhere',
+      userRank: 'guest',
+    }])));
+  });
 });
