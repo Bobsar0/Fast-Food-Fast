3c3e393504de0668239254b129362c778d96ac8a
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-07 04:01:16 +0100

    Update controller to pass parameter verification test

--- api/v1/controllers/ordersController.js
+++ api/v1/controllers/ordersController.js
@@ -1,15 +1,20 @@
 import _ from 'lodash';
 
 export default class {
-  constructor(client) {
+  constructor(client, username, email) {
     this.client = client;
+    this.username = username;
+    this.email = email;
   }
 
   // GET /orders
   // index returns list of orders merged with corresponding ids
   index() {
     return this.client
-      .search()
+      .search({
+        user: this.username,
+        email: this.email,
+      })
       .then(res => _.map(res.orders, order => _.merge(order, { orderId: order.id })));
   }
 }
