3ad63f5b931c23e6dee7204261b827528790c730
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-07 14:12:49 +0100

    Controller CRUD implementation (#22)
    * Create failing test for index method of ordersController
    
    Index method should returns list of orders - GET /orders
    
    * Implement index method on ordersController to pass  test
    
    * Test that correct parameters are passed to client
    
    Verify that params are specified properly when client.search() is called
    
    * Update controller to pass parameter verification test
    
    * Modify server routes to include api version
    
    * Add hosting config file
    
    * Initialize Client class and update index file
    
    * Write failing test  for CREATE method of ordersController
    
    * Implement CREATE method on ordersController to pass test
    
    * Add test to verify that proper parameters are passed to client
    
    * Modify controller to pass parameter verification test
    
    * Write failing test for READ/id method of ordersController
    
    Include parameter verification test
    
    * Implement READ method on ordersController to pass tests
    
    * Write text to check for non-existent ids
    
    * Modify READ method on controller to pass tests
    
    * Write failing test for UPDATE/id method of ordersController
    
    Include parameter verification test
    
    * Implement UPDATE method on ordersController to pass tests
    
    * Write failing test for DELETE/id method of ordersController
    
    Include parameter verification and id check
    
    * Implement CANCEL method on ordersController to pass tests
    
    * Fix bug in test
    
    * Change method 'de'l to 'cancel' to reflct controller method name change
    
    'Cancel' affords the reader more clarity on the method function
    
    * Update client to test API functionality
    
    * Modify server to display response in JSON format

--- api/v1/client.js
+++ api/v1/client.js
@@ -6,19 +6,18 @@ class Client {
     this.host = host;
     this.path = path;
   }
-
-  search(username, attr) {
-    this.username = username;
-    this.attr = attr;
-    return this;
-  }
 }
 
 const client = new Client({
   host: `${config.local.host}:${config.local.port}`,
   path: 'api/v1/',
 });
+// TEST data is empty for now. Just to test the API
 const data = {};
 client.search = () => new Promise((resolve, reject) => resolve(data).catch(reject));
+client.save = () => new Promise((resolve, reject) => resolve(data).catch(reject));
+client.get = () => new Promise((resolve, reject) => resolve(data).catch(reject));
+client.update = () => new Promise((resolve, reject) => resolve(data).catch(reject));
+client.delete = () => new Promise((resolve, reject) => resolve(data).catch(reject));
 
 export default client;
--- api/v1/server.js
+++ api/v1/server.js
@@ -8,22 +8,24 @@ export default (orders) => {
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
   // GET /orders
+  server.get(`${prefix}/orders/`, (req, res) => orders.index().then(result => res.status(200).json(result)));
   server.get(`${prefix}/orders/`, (req, res) => orders.index().then(result => res.status(200).send(result)));
+  
   // GET /orders:orderId
   server.get(`${prefix}/orders/:orderId`, (req, res) => orders.read(req.params.orderId)
     .then(result => res.status(200).send(result))
     .catch(() => res.sendStatus(404)));
 
   // POST /orders
-  server.post(`${prefix}/orders/`, (req, res) => orders.create(req.body.order).then(result => res.status(201).send(result)));
+  server.post(`${prefix}/orders/`, (req, res) => orders.create(req.body.order).then(result => res.status(201).json(result)));
   // PUT /orders/:orderId
   server.post(`${prefix}/orders/:orderId`, (req, res) => orders.update(req.params.orderId, req.body.order)
-    .then(result => res.status(200).send(result))
+    .then(result => res.status(200).json(result))
     .catch(() => res.sendStatus(404)));
 
   // DELETE /orders/:orderId
-  server.delete(`${prefix}/orders/:orderId`, (req, res) => orders.del(req.params.orderId)
-    .then(() => res.status(200).send({ orderId: req.params.orderId }))
+  server.delete(`${prefix}/orders/:orderId`, (req, res) => orders.cancel(req.params.orderId)
+    .then(() => res.status(200).json({ orderId: req.params.orderId }))
     .catch(() => res.sendStatus(404)));
   return server;
 };
