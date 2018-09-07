4bd6b59cff7c92869c5549412b39807ef0409690
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-07 07:59:39 +0100

    Write failing test  for CREATE method of ordersController

--- test/ordersControllerTest.js
+++ test/ordersControllerTest.js
@@ -1,16 +1,19 @@
+import 'babel-polyfill';
 import sinon from 'sinon';
 import 'should-sinon';
+import _ from 'lodash';
 import OrdersController from '../api/v1/controllers/ordersController';
 
 describe('OrdersController', () => {
   // introdue external client stub
   const client = {};
-  const orders = new OrdersController(client, 'Steve', 'bobsarglobal@gmail.com');
+  const orders = new OrdersController(client, 'Steve', 'orders');
 
+  // GET /orders test
   // index should return list of orders merged with corresponding ids
   describe('index', () => {
     before(() => {
-      client.search = () => new Promise((resolve, reject) => resolve({
+      client.search = () => new Promise(resolve => resolve({
         orders: [{
           orderId: 'ABCDEFGHIJKLMNO',
           name: 'Chicken',
@@ -47,17 +50,34 @@ describe('OrdersController', () => {
       userRank: 'guest',
     }])));
 
-    it('specifies proper username and email while searching', () => {
+    it('specifies proper username and attr while searching', () => {
       // import sinon for spying on method calls
       const spy = sinon.spy(client, 'search');
-      // search() method should be called once with proper username and email as paramters.
+      // search() method should be called once with proper username and attributes as paramters.
       return orders.index().then(() => {
         spy.should.be.calledOnce();
         spy.should.be.calledWith({
           user: 'Steve',
-          email: 'bobsarglobal@gmail.com',
+          attr: 'orders',
         });
       });
+    });
+  });
+  // POST /orders test
+  // create should establish a new order
+  describe('create', () => {
+    const attrs = {
+      name: 'Cocktail', price: '1200', username: 'Steve', userAddr: 'Andela Epic Tower',
+    };
+    before(() => {
+      client.save = () => new Promise(resolve => resolve({
+        store: 'orderStore',
+        orderId: 'ABCDEFGHIJKLMNO',
+        created: true,
+      }));
+    });
+    it('parses and returns order data', () => {
+      orders.create(attrs).then(result => result.should.deepEqual(_.merge({ orderId: 'ABCDEFGHIJKLMNO' }, attrs)));
     });
   });
 });
