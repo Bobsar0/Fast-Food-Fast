74947ec5a1ffc7f05d6e19d2393fb3d5de355639
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-07 18:33:30 +0100

    Set up server to power  UI

--- api/v1/server.js
+++ api/v1/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 // OrdersController intance must be created and passed from outside
 export default (orders) => {
@@ -10,7 +11,7 @@ export default (orders) => {
   // GET /orders
   server.get(`${prefix}/orders/`, (req, res) => orders.index().then(result => res.status(200).json(result)));
   server.get(`${prefix}/orders/`, (req, res) => orders.index().then(result => res.status(200).send(result)));
-  
+
   // GET /orders:orderId
   server.get(`${prefix}/orders/:orderId`, (req, res) => orders.read(req.params.orderId)
     .then(result => res.status(200).send(result))
@@ -27,5 +28,30 @@ export default (orders) => {
   server.delete(`${prefix}/orders/:orderId`, (req, res) => orders.cancel(req.params.orderId)
     .then(() => res.status(200).json({ orderId: req.params.orderId }))
     .catch(() => res.sendStatus(404)));
+
+  // ==========POWER FRONT-END PAGES===============//
+  const dirName = '/UI/templates';
+  server.use(express.static(path.join(__dirname, '/UI')));
+
+  server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, dirName, '/index.html'));
+  });
+  server.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, dirName, '/index.html'));
+  });
+  server.get('/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, `${dirName}/menu.html`));
+  });
+
+  server.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, `${dirName}/signup.html`));
+  });
+  server.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, `${dirName}/login.html`));
+  });
+  server.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, `${dirName}/admin.html`));
+  });
+
   return server;
 };
