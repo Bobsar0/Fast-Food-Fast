c633406bcd58d6830e194a80acc43079820d01ce
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-26 04:26:42 +0100

    Include model scripts and delete unnecessary ones.

--- server/api/v1/models/orderModel.js
+++ server/api/v1/models/orderModel.js
@@ -18,9 +18,6 @@ export default class OrderModel {
   // get order from store based on id
   get(id) {
     return new Promise((resolve, reject) => {
-      if (this.store.length === 0) {
-        return reject(id);
-      }
       this.store.forEach((order) => {
         if (order.orderId === id) {
           return resolve({
@@ -29,6 +26,7 @@ export default class OrderModel {
             value: order,
           });
         }
+        return reject(id);
       });
       return reject(id);
     });
