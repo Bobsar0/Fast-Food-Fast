b2f476bf25dc14d6340f2dda2966348a90b17906
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-09-27 10:19:32 +0100

    Add sticky footer

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -11,22 +11,19 @@ icon[0].onclick = () => {
 
 // Add sticky menu navbar and footer
 const nav = document.querySelector('nav');
-// const footer = document.querySelector('footer');
+const footer = document.querySelector('footer');
 // Get the offset position of the menuNav
 const stickyNav = nav.offsetTop;
-// const stickyFooter = footer.offsetTop;
-console.log('nav off: ', stickyNav)
-// console.log('nav off footer: ', stickyFooter)
-
 
 window.onscroll = () => {
   // Add the sticky class to the menuNav when you reach its scroll position.
   if (window.pageYOffset >= stickyNav) {
     nav.classList.add('stickyNav');
-    // footer.classList.add('stickyFooter');
+    // Add a sticky footer
+    footer.style.position = 'fixed';
   } else {
     // Remove "sticky" when you leave the scroll position
     nav.classList.remove('stickyNav');
-    // footer.classList.remove('stickyFooter');
+    footer.style.position = 'relative';
   }
 };
