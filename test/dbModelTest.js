d0b7dd1b2b4eac865b4db3c2cfc6e5aba06dfb72
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-02 15:21:31 +0100

    Write tests that fail for db model

---
+++ test/dbModelTest.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import DB from '../server/api/models/dbModel';
+
+dotenv.config();
+
+describe('Database Model', () => {
+  const pool = new Pool({
+    connectionString: process.env.DB_URL_TEST,
+  });
+  before(() => {
+    pool.on('connect', () => {
+    });
+  });
+  after(() => {
+    pool.end();
+  });
+  const db = new DB(pool);
+  it('creates a new model', () => {
+    expect(db).to.be.an.instanceof(DB);
+  });
+  it('takes in connection pool as a paramater', () => {
+    expect(db).to.have.property('pool');
+  });
+});
