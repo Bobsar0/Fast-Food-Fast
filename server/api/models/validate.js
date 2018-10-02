8733cd0c9797c56b59040930d2fd4cb94a827165
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-02 19:56:46 +0100

    Implement validator class to pass tests

---
+++ server/api/models/validate.js
@@ -0,0 +1,8 @@
+
+// Adapted from https://www.codementor.io/olawalealadeusi896/building-a-simple-api-with-nodejs-expressjs-postgresql-db-and-jwt-3-mke10c5c5
+export default class Validator {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+}
