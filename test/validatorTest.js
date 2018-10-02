c9dfb309bf22ca44855f61c3fce995c3b7a078ae
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-02 21:35:44 +0100

    Add failing tests for  email validator

--- test/validatorTest.js
+++ test/validatorTest.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import Validator from '../server/api/models/validate';
+import Validator from '../server/api/models/userValidator';
 
 describe('Validator', () => {
   it('creates a new model', () => {
@@ -8,5 +8,18 @@ describe('Validator', () => {
   it('takes in email and password as paramaters', () => {
     expect(new Validator()).to.have.property('email');
     expect(new Validator()).to.have.property('password');
+  });
+
+  describe('checks for valid email', () => {
+    it('returns false for invalid email', () => {
+      const email = 'bob';
+      const user = new Validator(email, '');
+      expect(user.isValidEmail()).to.equal(false);
+    });
+    it('returns true for valid email', () => {
+      const email = 'bob@gmail.com';
+      const user = new Validator(email, '');
+      expect(user.isValidEmail()).to.equal(true);
+    });
   });
 });
