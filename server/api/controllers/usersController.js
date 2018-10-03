f37895bbcef55ab4c94b3f429f936a18f76e730f
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-03 02:14:26 +0100

    create user's controller  class to pass tests

---
+++ server/api/controllers/usersController.js
@@ -0,0 +1,11 @@
+/**
+   * User Controller
+   * @param {object} db
+   * @param {instance} Usermodel
+   */
+export default class {
+  constructor(db, userModel) {
+    this.db = db;
+    this.user = userModel;
+  }
+}
