c68f7a3bddfab7136d0960ccdf52a9b99c5ff5cd
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-03 19:20:45 +0100

    Update user controller to use destructuring

--- server/api/controllers/usersController.js
+++ server/api/controllers/usersController.js
@@ -1,12 +1,13 @@
 /**
    * User Controller
    * @param {object} db
-   * @param {instance} Usermodel
+   * @param {instance} user
    */
 export default class {
-  constructor(db, userModel) {
+  constructor(db, user, auth) {
     this.db = db;
-    this.user = userModel;
+    this.user = user;
+    this.auth = auth;
   }
 
   /**
@@ -15,19 +16,19 @@ export default class {
      * @returns {object} user object
      */
   async create(req) {
-    if (!req.body.username || !req.body.email || !req.body.password) {
+    const { email, username, password } = req.body;
+    if (!username || !email || !password) {
       return { status: 400, message: 'Please input username, email and password' };
     }
-    if (!req.body.username) {
+    if (!username) {
       return { status: 400, message: 'Please input username' };
     }
-    if (!req.body.email) {
+    if (!email) {
       return { status: 400, message: 'Please input email' };
     }
-    if (!req.body.password) {
+    if (!password) {
       return { status: 400, message: 'Please input password' };
     }
-
     this.user = Object.assign(this.user, req.body);
 
     if (!this.user.isValidEmail()) {
@@ -36,6 +37,7 @@ export default class {
     if (this.user.isValidPassword() !== 'true') {
       return { status: 400, message: this.user.isValidPassword() };
     }
+    this.user.password = this.auth.hashPassword(this.user.password);
 
     const createQuery = `INSERT INTO
           users(username, email, password, phone, address, rank, created_date, modified_date)
@@ -44,7 +46,7 @@ export default class {
     const values = [
       this.user.username,
       this.user.email,
-      this.user.hashedPassword,
+      this.user.password,
       this.user.phone,
       this.user.address,
       this.user.rank,
@@ -54,13 +56,15 @@ export default class {
     try {
       const { rows } = await this.db.query(createQuery, values);
       this.user.userId = rows[0].userid;
-      const token = this.user.generateToken(rows[0].userid, rows[0].rank);
-      return { status: 201, message: 'user account successfully created', token };
+      const token = this.auth.generateToken(rows[0].userid, rows[0].rank);
+      return {
+        status: 201, message: 'user account successfully created', user: this.user, token,
+      };
     } catch (error) {
       if (error.routine === '_bt_check_unique') {
         return { status: 400, message: error.detail };
       }
-      return { status: 400, error: error.message };
+      return { status: 400, error };
     }
   }
 }
