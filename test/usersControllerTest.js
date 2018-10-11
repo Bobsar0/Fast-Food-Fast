74b329336bdb187cd321c02931b90bde9a53c42f
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-11 12:34:20 +0100

    add more user endpoint tests to improve code coverage

--- test/usersControllerTest.js
+++ test/usersControllerTest.js
@@ -46,8 +46,126 @@ describe('User Endpoints', () => {
   });
 
   describe('POST /auth/signup', () => {
-    it('it should successfully create a user account', (done) => {
-      const user = { username: 'bobo', email: 'bobo@gmail.com', password: 'Password!2' };
+    it('does not create user account with null username, email or password', (done) => {
+      const user = { username: 'bobo' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Please input username, email and password');
+          done();
+        });
+    });
+    it('does not create user account with invalid username', (done) => {
+      const user = { username: ' ', email: 'bob@gmail.com', password: 'Abc!23' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Please input valid username');
+          done();
+        });
+    });
+    it('does not create user account with invalid email', (done) => {
+      const user = { username: 'bobo', email: 'bobo@g', password: 'Password!2' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Please enter a valid email address');
+          done();
+        });
+    });
+
+    it('does not create user account with password that does not contain an uppercase letter', (done) => {
+      const user = { username: 'bobo', email: 'bobo@gmail.com', password: 'password!2' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Your password must contain at least one uppercase letter');
+          done();
+        });
+    });
+
+    it('does not create user account with password that does not contain a lowercase letter', (done) => {
+      const user = { username: 'bobo', email: 'bobo@gmail.com', password: 'PASSWORD!2' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Your password must contain at least one lowercase letter');
+          done();
+        });
+    });
+
+    it('does not create user account with password that does not contain a number', (done) => {
+      const user = { username: 'bobo', email: 'bobo@gmail.com', password: 'Password!' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Your password must contain at least one number');
+          done();
+        });
+    });
+    it('does not create user account with password that does not contain a special character', (done) => {
+      const user = { username: 'bobo', email: 'bobo@gmail.com', password: 'Password12' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Your password must contain at least one of these special characters: @, $, !, %, *, ?, &');
+          done();
+        });
+    });
+
+    it('does not create user account with password that does not contain up to six characters', (done) => {
+      const user = { username: 'bobo', email: 'bobo@gmail.com', password: 'Pas!2' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Your password must be composed of at least 6 characters');
+          done();
+        });
+    });
+
+    it('catches all other errors', (done) => {
+      const user = {
+        username: 'bobo', email: 'bobo@gmail.com', password: 'Pass!2', phone: '012345678901234567890',
+      };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(500);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
+
+    it('it should successfully create a user account with valid credentials', (done) => {
+      const user = {
+        username: 'Bobo', email: 'BOBO@gmail.com', password: 'Password!2', phone: '0123456789',
+      };
       chai.request(server(orderC, userC, menuC))
         .post('/api/v1/auth/signup')
         .send(user)
@@ -60,16 +178,58 @@ describe('User Endpoints', () => {
           res.body.should.have.property('message').eql('Signup successful');
           res.body.user.should.have.property('email').eql('bobo@gmail.com');
           res.body.user.should.have.property('username').eql('bobo');
+          res.body.user.should.have.property('phone').eql('0123456789');
           res.body.user.should.have.property('createdDate');
           res.body.user.should.have.property('modifiedDate');
           res.body.should.have.property('token');
           done();
         });
     });
+
+    it('does not create user account with already existing username', (done) => {
+      const user = {
+        username: 'Bobo', email: 'bob@gmail.com', password: 'Abc!23',
+      };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(409);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Username bobo already exists');
+          done();
+        });
+    });
+    it('does not create user account with already existing email address', (done) => {
+      const user = { username: 'B', email: 'bobo@gmail.com', password: 'Abc!23' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(409);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Email address bobo@gmail.com already exists');
+          done();
+        });
+    });
+    it('does not create user account with already existing phone number', (done) => {
+      const user = {
+        username: 'bobsar', email: 'bobsar@gmail.com', password: 'Abc!23', phone: '0123456789',
+      };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(409);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Phone number 0123456789 already exists');
+          done();
+        });
+    });
   });
 
   describe('POST /auth/login', () => {
-    it('it should not login a user with incorrect credentials', (done) => {
+    it('should not login a user with null or incorrect username or email and password', (done) => {
       const user = { username: 'foo', password: 'bar' };
       chai.request(server(orderC, userC, menuC))
         .post('/api/v1/auth/login')
@@ -81,26 +241,69 @@ describe('User Endpoints', () => {
         });
     });
 
-    it('it should successfully login an existing user with username and password', (done) => {
+    it('should not login a user with any information other than username/email) and password', (done) => {
+      const user = { username: 'foo', password: 'bar', role: 'admin' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/login')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('Please login with either (username or email) and password');
+          done();
+        });
+    });
+
+    it('should not login a user with correct username/email but incorrect password', (done) => {
+      const user = { username: 'bobo', password: 'bar' };
+      chai.request(server(orderC, userC, menuC))
+        .post('/api/v1/auth/login')
+        .send(user)
+        .end((err, res) => {
+          res.status.should.equal(400);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('The credentials you provided are incorrect');
+          done();
+        });
+    });
+
+    // it('catches all other errors', (done) => {
+    //   const user = {
+    //     username: 'bobo', email: 'bobo@gmail.com', password: 'Pass!2', phone: '01234890',
+    //   };
+    //   chai.request(server(orderC, userC, menuC))
+    //     .post('/api/v1/auth/login')
+    //     // .send(user)
+    //     .end((err, res) => {
+    //       res.status.should.equal(500);
+    //       res.body.status.should.equal('fail');
+    //       res.body.should.have.property('error');
+    //       done();
+    //     });
+    // });
+
+    it('should successfully login an existing user with valid username and password', (done) => {
       const user = { username: 'bobo', password: 'Password!2' };
       chai.request(server(orderC, userC, menuC))
         .post('/api/v1/auth/login')
         .send(user)
         .end((err, res) => {
           res.status.should.equal(200);
+          res.body.status.should.equal('success');
           res.body.should.have.property('message').eql('Login successful');
           res.body.should.have.property('token');
           done();
         });
     });
 
-    it('it should successfully login an existing user with email and password', (done) => {
+    it('should successfully login an existing user with valid email and password', (done) => {
       const user = { email: 'bobo@gmail.com', password: 'Password!2' };
       chai.request(server(orderC, userC, menuC))
         .post('/api/v1/auth/login')
         .send(user)
         .end((err, res) => {
           res.status.should.equal(200);
+          res.body.status.should.equal('success');
           res.body.should.have.property('message').eql('Login successful');
           res.body.should.have.property('token');
           done();
@@ -109,14 +312,49 @@ describe('User Endpoints', () => {
   });
 
   describe('GET /users/userId/orders', () => {
-    it('it should GET the order history of a particular user', (done) => {
+    it('it returns failure message for invalid userId', (done) => {
+      chai.request(server(orderC, userC, menuC))
+        .get('/api/v1/users/10000/orders')
+        .set({ 'x-access-token': testToken })
+        .end((err, res) => {
+          res.status.should.equal(404);
+          res.body.status.should.equal('fail');
+          res.body.should.have.property('message').eql('User not found');
+          done();
+        });
+    });
+    it('it returns failure message for null token', (done) => {
+      chai.request(server(orderC, userC, menuC))
+        .get('/api/v1/users/10000/orders')
+        .end((err, res) => {
+          res.status.should.equal(401);
+          res.body.should.have.property('error');
+          res.body.error.status.should.equal('fail');
+          res.body.error.should.have.property('message').eql('Token is not provided');
+          done();
+        });
+    });
+    it('catches all other errors', (done) => {
+      chai.request(server(orderC, userC, menuC))
+        .get(`/api/v1/users/${id}/orders`)
+        .set({ 'x-access-token': 'invalid' })
+        .end((err, res) => {
+          res.status.should.equal(500);
+          res.body.should.have.property('error');
+          res.body.error.status.should.equal('fail');
+          res.body.error.should.have.property('message').eql('jwt malformed');
+          done();
+        });
+    });
+    it('should GET the order history of a particular user', (done) => {
       chai.request(server(orderC, userC, menuC))
         .get(`/api/v1/users/${id}/orders`)
         .set({ 'x-access-token': testToken })
         .end((err, res) => {
           res.status.should.equal(200);
           res.body.status.should.equal('success');
-          res.body.should.have.property('message');
+          res.body.should.have.property('orders');
+          res.body.should.have.property('message').eql('User has not made an order yet');
           done();
         });
     });
