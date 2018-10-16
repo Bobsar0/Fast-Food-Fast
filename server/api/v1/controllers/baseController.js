33ea5155f6571a150537874b755e9ddb0484132e
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-16 01:15:10 +0100

    delete api-specific files

--- server/api/v1/controllers/baseController.js
+++
@@ -1,7 +0,0 @@
-export default class BaseController {
-  constructor(client, username, attr) {
-    this.client = client;
-    this.username = username;
-    this.attr = attr;
-  }
-}
--- test/baseModelTest.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import BaseModel from '../server/api/v1/models/baseModel';
-
-describe('Models', () => {
-  it('should create a new model', () => {
-    const UserModel = new BaseModel();
-    expect(UserModel).to.be.an.instanceof(BaseModel);
-  });
-});
