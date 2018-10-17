3c428f6e8490165f1b05b797ac8c8f454a184d8d
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-17 10:44:20 +0100

    modify username script to suit pages with single username instance
    Also revert to default menu page when username isn't found

--- UI/app/js/username.js
+++ UI/app/js/username.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const meArr = document.getElementsByClassName('me');
   const name = localStorage.getItem('username');
-  const me = document.getElementsByClassName('me');
-  me[0].innerHTML = `<i class="fa fa-user fa-1x" aria-hidden="true"></i>${name}`;
-  me[1].textContent = name;
+  if (name) {
+    meArr[0].innerHTML = `<i class="fa fa-user fa-1x" aria-hidden="true"></i>${name}`;
+    if (meArr.length > 1) {
+      [...meArr].slice(1).forEach((me) => {
+        me.textContent = name;
+      });
+    }
+  } else {
+    window.location.href = '../../templates/menu.html';
+  }
 });
