9bd34f3990f5283307b96dc4bcb8963ea4fe80f9
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-17 14:18:50 +0100

    comment seemingly unnecessary db connection

--- server/api/controllers/authController.js
+++ server/api/controllers/authController.js
@@ -1,26 +1,26 @@
 import jwt from 'jsonwebtoken';
-import { Pool } from 'pg';
-import DB from '../models/dbModel';
+// import { Pool } from 'pg';
+// import DB from '../models/dbModel';
 
 
-let connectionString = '';
-let ssl = false;
+// let connectionString = '';
+// let ssl = false;
 
-if (process.env.NODE_ENV === 'test') {
-  connectionString = 'postgres://cfsezloo:oA41pLZTXNtBIR_vxJHO-ZXqwHM0lAzR@tantor.db.elephantsql.com:5432/cfsezloo';
-} else if (process.env.NODE_ENV === 'local') {
-  connectionString = process.env.DB_URL_LOCAL;
-} else {
-  // For Heroku
-  connectionString = process.env.DATABASE_URL;
-  ssl = true;
-}
-const pool = new Pool({
-  connectionString,
-  ssl,
-});
+// if (process.env.NODE_ENV === 'test') {
+//   connectionString = 'postgres://cfsezloo:oA41pLZTXNtBIR_vxJHO-ZXqwHM0lAzR@tantor.db.elephantsql.com:5432/cfsezloo';
+// } else if (process.env.NODE_ENV === 'local') {
+//   connectionString = process.env.DB_URL_LOCAL;
+// } else {
+//   // For Heroku
+//   connectionString = process.env.DATABASE_URL;
+//   ssl = true;
+// }
+// const pool = new Pool({
+//   connectionString,
+//   ssl,
+// });
 
-const db = new DB(pool);
+// const db = new DB(pool);
 const authController = {
   // Adapted from https://www.codementor.io/olawalealadeusi896/building-a-simple-api-with-nodejs-expressjs-postgresql-db-and-jwt-3-mke10c5c5
   /**
@@ -37,11 +37,12 @@ const authController = {
     }
     try {
       const decoded = await jwt.verify(token, 'fastFoodFast');
-      const text = 'SELECT * FROM users WHERE userid = $1';
-      const { rows } = await db.query(text, [decoded.userId]);
-      if (!rows[0]) {
-        return res.status(404).json({ error: { status: 'fail', statusCode: 404, message: 'User not found' } });
-      }
+      // const text = 'SELECT * FROM users WHERE userid = $1';
+      // const { rows } = await db.query(text, [decoded.userId]);
+      // if (!rows[0]) {
+      //   return res.status(404).json(
+      //  { error: { status: 'fail', statusCode: 404, message: 'User not found' } });
+      // }
       req.user = { userId: decoded.userId };
       return next();
     } catch (error) {
@@ -60,11 +61,12 @@ const authController = {
       if (decoded.role !== 'admin') {
         return res.status(403).json({ status: 'fail', statusCode: 403, message: 'Sorry, only admins are authorized' });
       }
-      const text = `SELECT * FROM users WHERE userid = ${decoded.userId}`;
-      const { rows } = await db.query(text);
-      if (!rows[0]) {
-        return res.status(404).json({ status: 'fail', statusCode: 404, message: 'Admin details not found' });
-      }
+      // const text = `SELECT * FROM users WHERE userid = ${decoded.userId}`;
+      // const { rows } = await db.query(text);
+      // if (!rows[0]) {
+      //   return res.status(404).json(
+      //  { status: 'fail', statusCode: 404, message: 'Admin details not found' });
+      // }
       req.user = { userId: decoded.userId };
       return next();
     } catch (error) {
