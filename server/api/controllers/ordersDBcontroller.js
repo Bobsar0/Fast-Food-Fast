74bc0fe294f4eda971ad95f2b7fd3440af889a85
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-10-20 03:34:02 +0100

    modify ordersController to process orders with multiple food items
    Controller only allowed single item orders. Enable multi-item orders

--- server/api/controllers/ordersDBcontroller.js
+++ server/api/controllers/ordersDBcontroller.js
@@ -9,47 +9,116 @@ export default class OrderDBController {
    * @param {object} order
    * @returns {object} order object
    */
+
   async create(req) {
     // The line below was adapted from this source: https://stackoverflow.com/questions/679915/how-do-i-test-for-an-empty-javascript-object
     if (Object.keys(req.body).length === 0 && req.body.constructor === Object) {
       return { status: 'fail', statusCode: 400, message: 'Sorry, order content cannot be empty' };
     }
-    const { name, quantity } = req.body;
-    if (!name || !name.trim() || typeof name !== 'string') {
-      return { status: 'fail', statusCode: 400, message: 'Please enter the name of your order' };
-    }
-    if (!quantity) {
-      return { status: 'fail', statusCode: 400, message: 'Please enter the quantity of your order' };
-    }
-    const itemName = name.trim().toUpperCase();
-    try {
-      const menuQuery = 'SELECT * from menu WHERE name = $1';
-      const response = await this.db.query(menuQuery, [itemName]);
-      if (response.rows.length === 0) {
-        return { status: 'fail', statusCode: 404, message: `Sorry, ${name} is not available on the menu` };
-      }
-      const totalPrice = Number(response.rows[0].price) * Number(quantity);
-      const query = `INSERT INTO orders(name, quantity, price, status, userid, created_date, modified_date)
-        VALUES($1, $2, $3, $4, $5, $6, $7)
-        returning *`;
-      const values = [
-        itemName,
-        quantity,
-        totalPrice,
-        'NEW',
-        req.user.userId,
-        new Date(),
-        new Date(),
-      ];
+    const {
+      name, quantity, foodArray, address, phone,
+    } = req.body;
+    let values = [];
 
-      const { rows } = await this.db.query(query, values);
-      return {
-        status: 'success', statusCode: 201, message: 'Order created successfully', order: rows[0],
-      };
-    } catch (error) {
-      if (error.routine === 'pg_atoi') {
-        return { status: 'fail', statusCode: 400, message: 'Please enter quantity in integer format' };
+    // FOR QUICK ORDER OF SINGLE FOOD
+    try {
+      if (Object.keys(req.body).length === 4 && name && quantity && address && phone) {
+        try {
+          if (!name.trim() || typeof name.trim() !== 'string') {
+            throw new Error('Please enter a valid name for your food');
+          }
+          const foodName = name.trim().toUpperCase();
+          const res = await this.db.query('SELECT * from menu WHERE food = $1', [foodName]);
+
+          if (res.rowCount === 0) {
+            throw new Error(`Sorry, ${foodName} is not available on the menu. Contact us on 08146509343 if needed urgently`);
+          }
+          const totalPrice = Number(res.rows[0].price) * Number(quantity);
+          values = [
+            foodName,
+            quantity,
+            totalPrice,
+            'NEW',
+            address,
+            phone,
+            req.user.userId,
+            new Date(),
+            new Date(),
+          ];
+        } catch (error) {
+          if (error.routine === 'pg_atoi') {
+            return { status: 'fail', statusCode: 400, message: 'Please enter quantity in integer format' };
+          }
+          return { status: 'fail', statusCode: 500, message: error.message };
+        }
+      // FOR ORDERS IN CART
+      } else if (Object.keys(req.body).length === 3 && foodArray && address && phone) {
+        let totalQty = 0;
+        let totalPrice = 0;
+
+        try {
+          const { rows } = await this.db.query('SELECT * FROM menu');
+          const foodNames = [];
+
+          rows.forEach((row) => {
+            foodNames.push(row.name);
+          });
+
+          await foodArray.forEach((food) => {
+            if (!food.name || typeof food.name !== 'string' || !food.name.trim()) {
+              throw new Error('Your cart object must have a \'name\' key of value type string');
+            }
+            if (!food.quantity || typeof food.quantity !== 'number') {
+              throw new Error('Your cart object must have a \'quantity\' key of value type integer > 0');
+            }
+            const foodName = food.name.trim().toUpperCase();
+            if (foodNames.indexOf(foodName) === -1) {
+              console.log('foodnames:', foodNames);
+              foodArray.splice(foodArray.indexOf(food), 1);
+              throw new Error(`Sorry, ${foodName} is not available in stock`);
+            }
+            const index = foodNames.indexOf(foodName);
+            totalQty += Number(food.quantity);
+            totalPrice += Number(rows[index].price) * Number(food.quantity);
+            if (foodArray.length === 0) {
+              throw new Error('Sorry, none of your ordered food tems are available on our menu');
+            }
+          });
+        } catch (error) {
+          return { status: 'fail', statusCode: 400, message: error.message };
+        }
+        values = [
+          JSON.stringify(foodArray),
+          totalQty,
+          totalPrice,
+          'NEW',
+          address,
+          phone,
+          req.user.userId,
+          new Date(),
+          new Date(),
+        ];
+      } else {
+        return { status: 'fail', statusCode: 400, message: 'Please place your order in the correct format. Refer to the API docs for more info.' };
       }
+
+      const insertQuery = `INSERT INTO orders(food, quantity, price, status, address, phone, userid, created_date, modified_date)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      returning *`;
+      try {
+        const { rows } = await this.db.query(insertQuery, values);
+        rows[0].food = JSON.parse(rows[0].food);
+        rows[0].orderid = `#${rows[0].userid}FFF${rows[0].orderid}`;
+        return {
+          status: 'success', statusCode: 201, message: 'Order created successfully', order: rows[0],
+        };
+      } catch (error) {
+        if (error.routine === 'pg_atoi') {
+          return { status: 'fail', statusCode: 400, message: 'Please enter quantity in integer format' };
+        }
+        return { status: 'fail', statusCode: 500, message: error.message };
+      }
+    } catch (error) {
       return { status: 'fail', statusCode: 500, message: error.message };
     }
   }
