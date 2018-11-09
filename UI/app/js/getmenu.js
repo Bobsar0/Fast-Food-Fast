c2e4e4d13e9bad71ca369a0f66888a30da5dfeb6
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-11-09 19:56:40 +0100

    #160788274 Implement pagination (#71)
    * add pagination script for menu pg2
    
    Only four food items are displayed per sectiom per page.
    
    * add pagination helper function
    
    * highlight only the current page
    
    * dynamically increase no of pages
    
    Increase no of pages as no of food items per section increases(4 items)
    
    * derive formula required to display 4 pgs at a time
    
    * remove static pgs leaving only the first pg
    
    * fix bug in page numbering
    
    Pg added to pgsArr overwrites existing pg as each uses the same 'a tag'
    
    * hide/show pagination during menu search
    
    * implement previous and next page scroll functionality

--- UI/app/js/getmenu.js
+++ UI/app/js/getmenu.js
@@ -12,6 +12,50 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const menuErr = document.getElementById('menuErr');
   const divsArr = [];
+  const sectionArr = [
+    { name: 'meals', foodDivs: [], count: 0 },
+    { name: 'snacks', foodDivs: [], count: 0 },
+    { name: 'drinks', foodDivs: [], count: 0 },
+    { name: 'combos', foodDivs: [], count: 0 },
+    { name: 'desserts', foodDivs: [], count: 0 },
+  ];
+
+  const pg1 = document.getElementById('page1');
+  const prevPg = document.getElementById('prevPg');
+  const nextPg = document.getElementById('nextPg');
+
+  const pgsArr = [pg1];
+  const pgsId = ['page1'];
+
+  function pagination(pgId, startIndex) {
+    sectionArr.forEach((genreSect) => {
+      document.getElementById(genreSect.name).style.display = 'none';
+      genreSect.foodDivs.forEach((div) => {
+        div.style.display = 'none';
+      });
+      // 4 food items per section
+      const divSlice = genreSect.foodDivs.slice(startIndex, startIndex + 4);
+      divSlice.forEach((div) => {
+        document.getElementById(genreSect.name).style.display = 'block';
+        div.style.display = 'block';
+      });
+    });
+    pgsArr.forEach((pg) => {
+      pg.className = '';
+    });
+    if (pgId === 'page1') {
+      document.getElementById(pgId).className = 'current';
+      prevPg.style.display = 'none';
+      nextPg.style.display = 'block';
+    } else if (pgId === `page${pgsArr.length}`) {
+      prevPg.style.display = 'block';
+      nextPg.style.display = 'none';
+    } else {
+      prevPg.style.display = 'block';
+      nextPg.style.display = 'block';
+    }
+  }
+
   fetch(req).then((resp) => {
     resp.json().then((res) => {
       if (res.status === 'success') {
@@ -66,10 +110,58 @@ document.addEventListener('DOMContentLoaded', () => {
                 <button class="cartBtn" id="btn${foodid}">Add to Cart</button>
               </div>
             </div>`;
-            const section = document.getElementById(`${genre}s`);
-            section.appendChild(div);
+
+            // Dynamically add pages based on no of food items per section
+            sectionArr.forEach((genreSect) => {
+              if (genreSect.name === `${genre}s`) {
+                genreSect.foodDivs.push(div);
+                const { length } = genreSect.foodDivs;
+                // 4 food items per section
+                if (length !== 1 && (length - 1) % 4 === 0) {
+                  const a = document.createElement('a');
+                  a.id = `page${((length - 1) / 4) + 1}`;
+                  a.textContent = ((length - 1) / 4) + 1;
+
+                  if (pgsId.indexOf(a.id) === -1) {
+                    document.getElementById('pgs').appendChild(a);
+                    pgsArr.push(a);
+                    pgsId.push(a.id);
+                  }
+                }
+              }
+            });
+            document.getElementById(`${genre}s`).appendChild(div);
             divsArr.push(div);
           }
+        });
+        pagination('page1', 0);
+
+        // Listen for a click event and display corresponding food items upon click
+        pgsArr.forEach((pg) => {
+          const { id } = pg;
+          document.getElementById(id).onclick = () => {
+            // Formula below displays 4 food items per section upon pg click
+            pagination(id, (id.slice(-1) ** 2) - ((id.slice(-1) - 2) ** 2));
+            pg.className = 'current';
+          };
+        });
+
+        [...document.getElementsByClassName('pgNav')].forEach((nav) => {
+          nav.onclick = () => {
+            let id = '';
+            pgsArr.forEach((pg) => {
+              if (pg.className.includes('current')) {
+                if (nav.id === 'prevPg') {
+                  id = `page${Number(pg.id.slice(-1)) - 1}`;
+                }
+                if (nav.id === 'nextPg') {
+                  id = `page${Number(pg.id.slice(-1)) + 1}`;
+                }
+                pagination(id, (id.slice(-1) ** 2) - ((id.slice(-1) - 2) ** 2));
+              }
+            });
+            document.getElementById(id).className = 'current';
+          };
         });
 
         let count = 0;
@@ -81,6 +173,8 @@ document.addEventListener('DOMContentLoaded', () => {
           [...document.querySelectorAll('section.menu')].forEach((section) => {
             section.style.display = 'none';
           });
+          // hide pagination
+          document.getElementById('pgsDiv').style.display = 'none';
           divsArr.forEach((div) => {
             const input = searchFood.value.toUpperCase();
             // show only relevant sections and divs
@@ -107,6 +201,11 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
               menuErr.className = 'success';
               menuErr.innerHTML = `${count} food items found`;
+            }
+            if (!input) {
+              // show pagination
+              document.getElementById('pgsDiv').style.display = 'block';
+              pagination(pg1.id, 0);
             }
           });
           if (count === totalFood) {
