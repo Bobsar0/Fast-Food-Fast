19f8a8b633ae69490f877c6b97ef002c4a46ccfb
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-11-16 13:08:32 +0100

    refactor piecharts

--- UI/app/js/piechart.js
+++ UI/app/js/piechart.js
@@ -81,55 +81,56 @@ fetch(req).then((resp) => {
   usersStatsErr.innerHTML = fetchErr;
 });
 
-req = new Request(`${host}/menu`, {
-  method: 'GET',
-  headers: {
-    'x-access-token': localStorage.token,
-  },
-});
-
 const genresArr = [];
 const genresObjArr = [];
 const menuArr = [['Genre', 'Total']];
 
-fetch(req).then((resp) => {
-  resp.json().then((res) => {
-    console.log('response:', res)
-    if (res.status === 'success') {
-      document.getElementById('menu').innerHTML = res.count;
-      res.products.forEach((food) => {
-        const { genre } = food;
-        if (genresArr.indexOf(genre) === -1) {
-          genresArr.push(genre);
-          genresObjArr.push({ name: genre, count: 0 });
-        }
-
-        genresObjArr.forEach((genreObj) => {
-          if (genreObj.name === genre) {
-            genreObj.count += 1;
-          }
-        });
-      });
-      genresObjArr.forEach((genreObj) => {
-        menuArr.push([genreObj.name, genreObj.count]);
-      });
-    } else {
-      menuStatsErr.innerHTML = res.message;
-    }
-  }).catch((err) => {
-    menuStatsErr.innerHTML = err;
+const getMenu = new Promise((resolve, reject) => {
+  req = new Request(`${host}/menu`, {
+    method: 'GET',
+    headers: {
+      'x-access-token': localStorage.token,
+    },
   });
-}).catch((fetchErr) => {
-  menuStatsErr.innerHTML = fetchErr;
-});
 
+  fetch(req).then((resp) => {
+    resp.json().then((res) => {
+      if (res.status === 'success') {
+        document.getElementById('menu').innerHTML = res.count;
+        res.products.forEach((food) => {
+          const { genre } = food;
+          if (genresArr.indexOf(genre) === -1) {
+            genresArr.push(genre);
+            genresObjArr.push({ name: genre, count: 0 });
+          }
+
+          genresObjArr.forEach((genreObj) => {
+            if (genreObj.name === genre) {
+              genreObj.count += 1;
+            }
+          });
+        });
+        genresObjArr.forEach((genreObj) => {
+          menuArr.push([genreObj.name, genreObj.count]);
+        });
+        resolve(menuArr);
+      } else {
+        menuStatsErr.innerHTML = res.message;
+      }
+    }).catch((err) => {
+      menuStatsErr.innerHTML = err;
+    });
+  }).catch((fetchErr) => {
+    menuStatsErr.innerHTML = fetchErr;
+  });
+});
 
 // DRAW CHARTS
 // Load google charts
 google.charts.load('current', { packages: ['corechart', 'bar'] });
 
-// Draw the chart and set the chart values
 function drawChart() {
+  // DRAW ORDERS PIE CHART
   const ordersData = google.visualization.arrayToDataTable([
     ['Status', 'Number of orders'],
     ['New', totalNew],
@@ -138,25 +139,22 @@ function drawChart() {
     ['Cancelled', totalCancelled],
     ['Complete', totalComplete],
   ]);
-
-  console.log('genre:', menuArr)
-  const menuData = google.visualization.arrayToDataTable(menuArr);
-
-  const salesData = google.visualization.arrayToDataTable([
-    ['Year', 'TotalSales', 'Meal', 'Snacks', 'Drinks', 'Combo', 'Desserts' ],
-    ['2014', 1000, 400, 200, 3,4,5],
-    ['2015', 1170, 460, 250, 55,55,66],
-    ['2016', 660, 1120, 300,44,5,6],
-    ['2017', 1030, 540, 350, 44, 5,6]
-  ]);
-
-  // Optional; add a title and set the width and height of the chart
   const ordersOptions = {
-    title: 'Orders Statistics', is3D: true, width: 400, height: 250,
+    title: 'Orders Statistics', is3D: true, width: 450, height: 220,
+
   };
-  const menuOptions = {
-    title: 'Menu Statistics', is3D: true, width: 400, height: 250,
-  };
+  const ordersChart = new google.visualization.PieChart(document.getElementById('ordersChart'));
+  ordersChart.draw(ordersData, ordersOptions);
+
+  // DRAw MENU PIE CHART
+  getMenu.then((menu) => {
+    const menuData = google.visualization.arrayToDataTable(menu);
+    const menuChart = new google.visualization.PieChart(document.getElementById('menuChart'));
+    const menuOptions = {
+      title: 'Menu Statistics', is3D: true, width: 450, height: 220,
+    };
+    menuChart.draw(menuData, menuOptions);
+  }).catch(err => console.log('error in getMenu:', err));
 
   const salesOptions = {
     chart: {
@@ -164,13 +162,14 @@ function drawChart() {
       subtitle: 'Sales across all Food genres: 2014-2017',
     },
   };
-  // Display the chart inside the <div> element with id="piechart"
-  const ordersChart = new google.visualization.PieChart(document.getElementById('ordersChart'));
-  const menuChart = new google.visualization.PieChart(document.getElementById('menuChart'));
+  const salesData = google.visualization.arrayToDataTable([
+    ['Year', 'TotalSales', 'Meal', 'Snacks', 'Drinks', 'Combo', 'Desserts' ],
+    ['2014', 1000, 400, 200, 3,4,5],
+    ['2015', 1170, 460, 250, 55,55,66],
+    ['2016', 660, 1120, 300,44,5,6],
+    ['2017', 1030, 540, 350, 44, 5,6]
+  ]);
   const salesChart = new google.charts.Bar(document.getElementById('salesGraph'));
-
-  ordersChart.draw(ordersData, ordersOptions);
-  menuChart.draw(menuData, menuOptions);
   salesChart.draw(salesData, google.charts.Bar.convertOptions(salesOptions));
 }
 
