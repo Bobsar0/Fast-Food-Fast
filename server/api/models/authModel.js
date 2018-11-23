57c045fe625dbbf548493b7d98121d1d9d33eff8
Steve Onyeneke <OBINNA@OBINNA-HP.(none)>
2018-11-23 12:17:02 +0100

    enable email delivery service

--- server/api/models/authModel.js
+++ server/api/models/authModel.js
@@ -33,7 +33,7 @@ export default class {
    * @returns {string} token
    */
   generateToken(id, username, email, role) {
-    this.token = jwt.sign({ userId: id, username, email, role }, 'fastFoodFast', { expiresIn: '7d' });
+    this.token = jwt.sign({ userId: id, username, email, role }, process.env.SECRET, { expiresIn: '7d' });
     return this.token;
   }
 }
